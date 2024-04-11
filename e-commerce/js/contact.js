680f64d192bb6094b29db5bfb71b7a099be7df95
Nicolas Corbin <corbin232@campus.unicaen.fr>
2024-04-11 15:01:10 +0200

    premier commit tp2

---
+++ e-commerce/js/contact.js
@@ -0,0 +1,15 @@
+// contact.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById('contactForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+
+        const email = document.getElementById('email').value
+
+    const vemail= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   
+    });
+        
+});
